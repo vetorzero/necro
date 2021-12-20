d103caf65b39527da9027a5c9882c42f5cd0ccde
Felds Liscia <dev@felds.com.br>
2021-12-20 12:20:39 -0300

    add credentials and region configs (wip)

---
+++ src/commands/config.ts
@@ -0,0 +1,73 @@
+import { Command } from "commander";
+import inquirer, { QuestionCollection, Separator } from "inquirer";
+import yaml from "yaml";
+
+// @todo add link to "https://console.aws.amazon.com/iam/home#/security_credentials"
+const questions: QuestionCollection = [
+  {
+    type: "list",
+    name: "_awsCredentials",
+    message: "AWS credentials",
+    choices: [
+      {
+        value: false,
+        name: "Use system credentials configured by AWS CLI",
+      },
+      { value: true, name: "Specify the user credentials for necro" },
+    ],
+  },
+  {
+    type: "input",
+    name: "aws.credentials.id",
+    message: "Access key ID",
+    when: (prev) => prev._awsCredentials,
+    validate: (answer: string) =>
+      answer.length === 20 ||
+      `The id must be 20 characters long. ${answer.length} given.`,
+  },
+  {
+    type: "password",
+    name: "aws.credentials.secret",
+    message: "Secret access key",
+    when: (prev) => prev._awsCredentials,
+    validate: (answer: string) =>
+      answer.length === 40 ||
+      `The secret must be 40 characters long. ${answer.length} given.`,
+  },
+  {
+    type: "list",
+    name: "_awsRegion",
+    message: "AWS region",
+    choices: [
+      {
+        value: false,
+        name: "Use default system region configured by AWS CLI",
+      },
+      { value: true, name: "Specify the region manually" },
+    ],
+  },
+  {
+    type: "input",
+    name: "aws.region",
+    message: "Region code",
+    when: (prev) => prev._awsRegion,
+    validate: (answer: string) =>
+      !!answer.match(/^\w+-\w+-\w+$/) ||
+      `Region must match the string format: ab-cde-1`,
+  },
+];
+
+export default function configCommand() {
+  return new Command("config")
+    .description("Configure necro environment.")
+    .action(action);
+}
+
+async function action(command: Command) {
+  const options = command.opts();
+  const answers = await inquirer.prompt(questions, options);
+
+  console.log(yaml.stringify(answers));
+
+  console.log({ answers });
+}
