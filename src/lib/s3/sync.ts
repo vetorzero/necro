fdf09374c20510594904f0b85b3a267e8e277b29
Felds Liscia <dev@felds.com.br>
2021-12-22 22:55:54 -0300

    add pretty logs to upload

--- src/lib/s3/sync.ts
+++ src/lib/s3/sync.ts
@@ -5,6 +5,8 @@ import { join } from "path";
 import { fileMd5 } from "../../utils/crypto";
 import _ from "lodash";
 import { createReadStream } from "fs";
+import chalk from "chalk";
+import { performance } from "perf_hooks";
 
 AWS.config.credentials = new AWS.SharedIniFileCredentials();
 AWS.config.region = "sa-east-1";
@@ -81,25 +83,30 @@ export async function sync(
   // const keepFiles = _.intersectionWith(remoteFiles, localFiles, _.isEqual);
   // console.log("keep", keepFiles);
 
+  console.time();
   const deleteFiles = _.differenceWith(remoteFiles, localFiles, _.isEqual);
-  console.log("delete", deleteFiles);
+  // console.log("delete", deleteFiles);
 
   const uploadFiles = _.differenceWith(localFiles, remoteFiles, _.isEqual);
-  console.log("upload", uploadFiles);
+  // console.log("upload", uploadFiles);
+  console.timeEnd();
 
   // upload new files
   for (const f of uploadFiles) {
-    console.log("Uplod:", bucket + "/" + targetDirWithSlash + f.path);
+    const startTime = performance.now();
+    process.stdout.write(chalk`{green ↑ ${f.path}...} `);
+
     const stream = createReadStream(join(sourceDir, f.path));
-    const req = {
-      Bucket: bucket,
-      Key: targetDirWithSlash + f.path,
-      Body: stream,
-    };
-    // console.log(req);
-    console.log("Uploading:", f.path);
-    await s3.upload(req).promise();
-    console.log("Done");
+    await s3
+      .upload({
+        Bucket: bucket,
+        Key: targetDirWithSlash + f.path,
+        Body: stream,
+      })
+      .promise();
+
+    const duration = performance.now() - startTime;
+    process.stdout.write(chalk`{green Done! (${duration.toFixed(0)}ms)}\n`);
   }
 
   // @todo delete files
