ff5a002834b85074efce9cf7cf51d8710dadc429
Felds Liscia <dev@felds.com.br>
2021-12-23 19:02:54 -0300

    refactor

--- src/lib/s3/sync.ts
+++ src/lib/s3/sync.ts
@@ -4,18 +4,20 @@ import fg from "fast-glob";
 import { join } from "path";
 import { fileMd5 } from "../../utils/crypto";
 import _ from "lodash";
-import { createReadStream } from "fs";
+import { copyFileSync, createReadStream } from "fs";
 import chalk from "chalk";
 import { performance } from "perf_hooks";
-
-AWS.config.credentials = new AWS.SharedIniFileCredentials();
-AWS.config.region = "sa-east-1";
 
 type FileRow = {
   path: string;
   md5?: string;
   isDir?: boolean;
 };
+
+AWS.config.credentials = new AWS.SharedIniFileCredentials();
+AWS.config.region = "sa-east-1";
+
+const s3 = new AWS.S3();
 
 export async function sync(
   sourceDir: string,
@@ -25,22 +27,40 @@ export async function sync(
 ) {
   console.log({ sourceDir, targetDir, bucket, meta });
 
-  const s3 = new AWS.S3();
+  // const s3 = new AWS.S3();
 
-  // list remote files
-  log.info(`Listing remote files...`);
-  const remoteFiles: FileRow[] = [];
   const targetDirWithSlash = targetDir.endsWith("/")
     ? targetDir
     : targetDir + "/";
 
+  const remoteFiles = _.sortBy(
+    await listRemoteFiles(bucket, targetDirWithSlash),
+    "path",
+  );
+  const localFiles = _.sortBy(await listLocalFiles(sourceDir), "path");
+
+  const filesToDelete = _.differenceWith(remoteFiles, localFiles, _.isEqual);
+  const filesToUpload = _.differenceWith(localFiles, remoteFiles, _.isEqual);
+
+  await deleteFiles(bucket, targetDirWithSlash, filesToDelete);
+  await uploadFiles(bucket, sourceDir, targetDirWithSlash, filesToUpload);
+
+  // @todo adjust meta
+  // @todo sync dirs (dont ignore on fetch; add on local glob)
+}
+
+async function listRemoteFiles(
+  bucket: string,
+  targetDir: string,
+): Promise<FileRow[]> {
+  const files: FileRow[] = [];
   let response: AWS.S3.ListObjectsV2Output | null = null;
   do {
     log.debug("Making request...");
     response = await s3
       .listObjectsV2({
         Bucket: bucket,
-        Prefix: targetDirWithSlash,
+        Prefix: targetDir,
         MaxKeys: 10,
         ContinuationToken:
           response && response.NextContinuationToken
@@ -51,23 +71,28 @@ export async function sync(
 
     response.Contents?.forEach((f) => {
       let isDir = false;
-      // if it's a dir. bail out for now.
+
+      // if it's a dir. bail out (for now).
       if (!f.Key || f.Key.endsWith("/")) {
         isDir = true;
         return;
       }
 
-      const path = f.Key?.slice(targetDirWithSlash.length);
-      remoteFiles.push({ path, md5: f.ETag?.slice(1, -1), isDir });
+      const path = f.Key?.slice(targetDir.length);
+      files.push({ path, md5: f.ETag?.slice(1, -1), isDir });
     });
   } while (response.IsTruncated);
 
-  // list local files
-  const localFiles: FileRow[] = [];
+  return files;
+}
+
+async function listLocalFiles(sourceDir: string): Promise<FileRow[]> {
+  const files: FileRow[] = [];
+
   const ls = await fg("**/*", { dot: true, cwd: sourceDir });
   await Promise.all(
     ls.map(async (path) => {
-      localFiles.push({
+      files.push({
         path,
         md5: await fileMd5(join(sourceDir, path)),
         isDir: false,
@@ -75,37 +100,37 @@ export async function sync(
     }),
   );
 
-  // console.log("remote", remoteFiles);
-  // console.log("local", localFiles);
+  return files;
+}
 
-  // compare files
-  // const keepFiles = _.intersectionWith(remoteFiles, localFiles, _.isEqual);
-  // console.log("keep", keepFiles);
-
-  const deleteFiles = _.differenceWith(remoteFiles, localFiles, _.isEqual);
-  // console.log("delete", deleteFiles);
-
-  const uploadFiles = _.differenceWith(localFiles, remoteFiles, _.isEqual);
-  // console.log("upload", uploadFiles);
-
-  // delete files
-  for (const f of deleteFiles) {
+async function deleteFiles(
+  bucket: string,
+  targetDir: string,
+  files: FileRow[],
+): Promise<void> {
+  for (const f of files) {
     const startTime = performance.now();
     process.stdout.write(chalk`{red ✕ ${f.path}...} `);
 
     await s3
       .deleteObject({
         Bucket: bucket,
-        Key: targetDirWithSlash + f.path,
+        Key: targetDir + f.path,
       })
       .promise();
 
     const duration = performance.now() - startTime;
     process.stdout.write(chalk`{red Done! (${duration.toFixed(0)}ms)}\n`);
   }
+}
 
-  // upload new files
-  for (const f of uploadFiles) {
+async function uploadFiles(
+  bucket: string,
+  sourceDir: string,
+  targetDir: string,
+  files: FileRow[],
+): Promise<void> {
+  for (const f of files) {
     const startTime = performance.now();
     process.stdout.write(chalk`{green ↑ ${f.path}...} `);
 
@@ -113,7 +138,7 @@ export async function sync(
     await s3
       .upload({
         Bucket: bucket,
-        Key: targetDirWithSlash + f.path,
+        Key: join(targetDir + f.path),
         Body: stream,
       })
       .promise();
@@ -121,7 +146,4 @@ export async function sync(
     const duration = performance.now() - startTime;
     process.stdout.write(chalk`{green Done! (${duration.toFixed(0)}ms)}\n`);
   }
-
-  // @todo adjust meta
-  // @todo sync dirs (dont ignore on fetch; add on local glob)
 }
