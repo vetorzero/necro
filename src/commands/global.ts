bab8fda695cc9fafbb8238debfe9019e74b3e427
Felds Liscia <dev@felds.com.br>
2022-02-09 18:36:51 -0300

    cleanup

--- src/commands/global.ts
+++ src/commands/global.ts
@@ -6,13 +6,6 @@ import config from "./global/config";
  */
 export default function global() {
   const cmd = new Command("global").storeOptionsAsProperties(false);
-  // .description(
-  //   "Get the URL for a demo. If no version is provided, show the link of the latest version.",
-  //   {
-  //     v: "The specific version of the demo.",
-  //   },
-  // );
-
   cmd.addCommand(config());
 
   return cmd;
