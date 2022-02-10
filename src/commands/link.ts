1b741a6d5bdd48935fd0ee4537d21dde64b32f5c
Felds Liscia <dev@felds.com.br>
2022-02-10 18:33:17 -0300

    update link command

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -1,8 +1,7 @@
-import assert from "assert";
 import chalk from "chalk";
 import { Command } from "commander";
-import { getConfig, ValidationError } from "../utils/config";
-import { error, success, warn } from "../utils/log";
+import { getConfig } from "../utils/config";
+import { success, warn } from "../utils/log";
 import { listDeployments } from "../utils/s3";
 
 export default function list() {
@@ -17,13 +16,13 @@ export default function list() {
 async function action(command: Command) {
   const config = await getConfig();
 
-  const bucket = config.aws?.hosting?.s3Bucket;
-  assert(bucket, "Bucket not defined.");
-
-  const deployments = await listDeployments(bucket, config.client, config.project);
+  const deployments = await listDeployments(
+    config.profile.hosting.s3_bucket,
+    config.client,
+    config.project,
+  );
 
   const { version } = command.opts();
-  // log(command.opts());
 
   const deployment = version
     ? deployments.reverse().find(x => x.name.startsWith(version))
@@ -33,8 +32,6 @@ async function action(command: Command) {
     success(`https://demo.vzlab.com.br/${deployment.prefix}`);
   } else {
     const cmd = chalk.bold("necro list");
-    warn(`Deployment not found.
-Use ${cmd} to get the available deployments.
-`);
+    warn(`Deployment not found.\nUse ${cmd} to get the available deployments.`);
   }
 }
