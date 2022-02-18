f662d9e0f324cbb6d384582a186533b11c1a5f59
Manoel S. Motoso <manoel.motoso@vzlab.com.br>
2022-02-18 12:07:04 -0300

    refactor, getProfile from utils

--- src/lib/config/merged.ts
+++ src/lib/config/merged.ts
@@ -1,34 +1,13 @@
-import assert from "assert";
-import chalk from "chalk";
-import { program } from "commander";
-import { GLOBAL_CONFIG_FILE } from "../../utils/config";
-import { info } from "../../utils/log";
-import { getGlobalConfig } from "./global";
+import { getProfile } from "../../utils/config";
 import { getProjectConfig } from "./project";
 
 export async function getConfig(): Promise<MergedConfig> {
   try {
-    const globalConfig = await getGlobalConfig();
     const projectConfig = await getProjectConfig();
-
-    const profile = await getProfile(globalConfig, projectConfig);
+    const profile = await getProfile();
 
     return { ...projectConfig, profile };
   } catch (err) {
     throw err;
   }
-}
-
-export async function getProfile(
-  globalConfig: GlobalConfig,
-  projectConfig: ProjectConfig,
-): Promise<GlobalConfig.Profile> {
-  const profileName =
-    program.opts().profile ?? projectConfig?.use_profile ?? globalConfig?.default_profile;
-  assert(profileName, `default_profile not configured in ${GLOBAL_CONFIG_FILE}`);
-
-  const profile = globalConfig?.profiles.find(p => profileName === p.name);
-  assert(profile, `profile ${profileName} does not exist.`);
-
-  return profile;
 }
